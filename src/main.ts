f8afe0d66854f12ccb88fae8e473d767098b06ce
anthhub <anthhub@163.com>
2019-11-29 18:23:24 +0800

    fix: rateLimit

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ async function bootstrap() {
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 1000, // limit each IP to 100 requests per windowMs
+      max: 1000 * 1000, // limit each IP to 100 requests per windowMs
     }),
   )
   app.use(logger)
